298d2f7b6e93fc8b49f74fa2e17196d5f85e1680
Aleksey Bershov <bershov.aleksey@bk.ru>
2025-03-27 11:57:52 +0300

    add @RequestParam

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,12 +3,15 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CarController {
 
     @GetMapping("/cars")
-    public String printCars() {
+    public String printCars(@RequestParam(value = "count", required = false) String count) {
+
+        System.out.println("count car: " + count);
         return "cars.html";
     }
 }
